3e60523eb68beeb7c09b22fb0693f3a0a702ff56
PrashamsaAdhikari <160519575+PrashamsaAdhikari@users.noreply.github.com>
2026-02-03 11:57:02 +0545

    Add files via upload

---
+++ 15function.py
@@ -0,0 +1,49 @@
+def function():
+    print("hihihihiihh")
+function()
+
+def add(a,b):
+    print(a+b)
+
+add (3,4)
+
+
+
+def sum (a,b):
+    return a+b
+
+num1=int(input("enter your first value"))
+num2=int(input("enter your second value"))
+
+res= sum(num1, num2)
+print("sum is", res)
+
+
+
+a=int(input("enter your first value: "))
+b=int(input("enter your second value: "))
+
+def add():
+    return a+b
+
+def sub():
+    return a-b
+
+def mul():
+    return a*b
+
+print("Sum is", add())
+print("Sum is", sub())
+print("Sum is", mul())
+
+
+
+
+
+
+
+
+
+
+
+num=int(input("enter your two values"))
---
+++ 16namemain.py
@@ -0,0 +1,7 @@
+#importing
+# def welcome():
+#     print("welcome from prashamsa")
+# welcome()
+
+import test
+print(test.add(2, 3))
