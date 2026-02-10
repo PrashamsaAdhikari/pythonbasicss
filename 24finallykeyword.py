6288480c1698f61878dc5b06c4deae97537e2df4
PrashamsaAdhikari <160519575+PrashamsaAdhikari@users.noreply.github.com>
2026-02-10 21:26:07 +0545

    finally keyword

---
+++ 24finallykeyword.py
@@ -0,0 +1,10 @@
+try:
+    l=[1,2,3,4,5]
+    i=int(input("enter the index: "))
+    print(l[i])
+except:
+    print("error error let's run sad sad")    
+finally:
+    print("Always executing sexy hahhahahh")
+
+ 
