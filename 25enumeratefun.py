61c611d10a4ee71a18a12e18c657e3f5209eb960
PrashamsaAdhikari <160519575+PrashamsaAdhikari@users.noreply.github.com>
2026-02-11 20:37:22 +0545

    enumerate() in Python

---
+++ 25enumeratefun.py
@@ -0,0 +1,13 @@
+
+marks = [12, 56, 32, 98, 12, 45, 1]
+index=0
+# for mark in marks:
+#     print(mark)
+#     if (index==3):
+#         print("good goood")
+#     index+=1    
+
+for index, mark in enumerate(marks):
+    print(mark)
+    if (index==3):
+        print("good goood")
